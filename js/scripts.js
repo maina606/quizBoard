bd0119cd2405186f85005f9b67c6148c2200e9ec
maina606 <cmmaina@gmail.com>
2019-02-25 08:11:20 +0300

    edits

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 /* ----------------------Business Logic - Functionality------------------------------*/
 var CorAns = ["script","funB","callFunB"];
-var CorAnsPoints = 10;
 
 function percentage(grade){
   return "You have scored " + parseInt((grade / 30) *100) + "%";
@@ -11,32 +10,24 @@ function percentage(grade){
 
 $(document).ready(function() {
   $("#queAns").submit(function(event) {//forms div
-
-    $("#results").text("");
-    var CorAnsPoints = 0;
+    var grade = 0
+    $("#results").show("");
+    var CorAnsPoints = 10;
     var queOneInput = ($("input[type=radio][name=queOne]:checked").val());
     var queTwoInput = ($("input[type=radio][name=fun]:checked").val());
     var queThreeInput = ($("input[type=radio][name=callFun]:checked").val());
+    debugger
+      if(queOneInput === CorAns[0]){
+        grade += CorAnsPoints;
+      }
+      if(queTwoInput === CorAns[1]){
+        grade += CorAnsPoints;
+      }
+      if(queThreeInput === CorAns[2]){
+        grade += CorAnsPoints;
+      }
 
-    if(queOneInput === undefined || queTwoInput === undefined || queThreeInput === undefined){
-      $("#Incomplete").text("Please complete the quiz");
-    } else {
-        if(queOneInput === CorAns[0]){
-          grade += CorAnsPoints;
-        }
-        if(queTwoInput === CorAns[1]){
-          grade += CorAnsPoints;
-        }
-        if(queThreeInput === CorAns[2]){
-          grade += CorAnsPoints;
-        }
-
-    $("input[type=radio][name=queOne]:checked").prop("checked", false);
-    $("input[type=radio][name=fun]:checked").prop("checked", false);
-    $("input[type=radio][name=callFun]:checked").prop("checked", false);
-    $("#Incomplete").text('');
-    $("#results").text(percentage(grade));
-}
+      $("#results h1").text(percentage(grade));
     event.preventDefault();
   });
 });
